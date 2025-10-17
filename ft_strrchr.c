008e281435e123eeced6c568b66c7fabfe7ff751
Oussama Korkech <okorkech@c3r2p7.1337.ma>
2025-10-17 14:34:52 +0100

    ;

---
+++ ft_strrchr.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   strrchr.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: okorkech <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/16 20:59:35 by okorkech          #+#    #+#             */
+/*   Updated: 2025/10/16 21:17:53 by okorkech         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strrchr(const char *s, int c)
+{
+	const char *end;
+
+	c = (char) c;
+	end = (s + ft_strlen(s));
+	while (end >= s)
+	{
+		if (*end == c)
+			return ((char *)end);
+		end--;
+	}
+	return (NULL);
+}
